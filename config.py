e1997d553c8b0fffff8a42818467dcb6b77daa69
YvesWang <yvestyw@gmail.com>
2018-11-28 11:39:59 -0500

    config

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ vocab_prefix = ['<PAD>', '<SOS>', '<EOS>', '<UNK>']
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 address_book1 = dict(
     train_src = 'Machine_Translation_NLP/iwsltzhen/iwslt-zh-en/train_sortByEn.tok.zh',
     train_tgt = 'Machine_Translation_NLP/iwsltzhen/iwslt-zh-en/train_sortByEn.tok.en',
@@ -33,10 +34,20 @@ address_book1 = dict(
 )
 
 address_book = dict(
-    train_src = 'Data/src_tokens',
-    train_tgt = 'Data/tgt_tokens',
+    train_src = 'Data/csrc_tokens',
+    train_tgt = 'Data/ctgt_tokens',
     val_src = 'Machine_Translation_NLP/iwsltzhen/iwslt-zh-en/dev.tok.zh',
     val_tgt = 'Machine_Translation_NLP/iwsltzhen/iwslt-zh-en/dev.tok.en',
     src_emb = 'embedding/wiki.zh.vec',
     tgt_emb = 'embedding/wiki.en.vec'
 )
+
+address_book1 = dict(
+    train_src = 'Data/src_tokens',
+    train_tgt = 'Data/tgt_tokens',
+    val_src = 'Data/src_tokens',
+    val_tgt = 'Data/tgt_tokens',
+    src_emb = 'embedding/wiki.zh.vec',
+    tgt_emb = 'embedding/wiki.en.vec'
+)
+
